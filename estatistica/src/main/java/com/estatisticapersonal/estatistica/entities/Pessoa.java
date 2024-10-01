897560fb5aa72c1cf2585a0b379b50d0b1d4313c
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-10-01 16:01:44 -0300

    create class e code pessoa

--- estatistica/src/main/java/com/estatisticapersonal/estatistica/entities/Pessoa.java
+++ estatistica/src/main/java/com/estatisticapersonal/estatistica/entities/Pessoa.java
@@ -1,5 +1,79 @@
 package com.estatisticapersonal.estatistica.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pessoa {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String biotipo;
+    private String experiencia;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int idade, String sexo, String biotipo, String experiencia) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.biotipo = biotipo;
+        this.experiencia = experiencia;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getBiotipo() {
+        return biotipo;
+    }
+
+    public void setBiotipo(String biotipo) {
+        this.biotipo = biotipo;
+    }
+
+    public String getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
+
 }
