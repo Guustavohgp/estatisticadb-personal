c44c2caf562a5dd0249fea9af2dfd485e87637b7
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-10-01 15:48:37 -0300

    create maven

---
+++ estatistica/src/main/java/com/estatisticapersonal/estatistica/EstatisticaApplication.java
@@ -0,0 +1,13 @@
+package com.estatisticapersonal.estatistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatisticaApplication.class, args);
+	}
+
+}
